d4eac8bbb6d132503c273a7510df3e276061269c
ayoubouakrim <ayoubnadorx1@gmail.com>
2024-11-24 13:07:14 +0100

    updating vite config code

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/navapp.js/app.navapp.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'  // Use the standard path
+            ],
             refresh: true,
         }),
     ],
