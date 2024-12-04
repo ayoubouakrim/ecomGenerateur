218604f22c86d883269ccd042e422834622d8307
ayoubouakrim <ayoubnadorx1@gmail.com>
2024-12-04 23:44:17 +0100

    fixing the css problem

--- resources/views/login/show.blade.php
+++ resources/views/login/show.blade.php
@@ -1,5 +1,5 @@
 @push('styles')
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login.css'm true) }}">
 @endpush
 <x-master title='login'>
 
